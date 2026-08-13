2220fe91000a211d11346e959055f76cf5e45ae6
Carlos Reis <carlosfmreis91@gmail.com>
2026-08-13 16:45:46 +0100

    ORM setup

---
+++ api/models/test.php
@@ -0,0 +1,7 @@
+<?php
+use Illuminate\Database\Eloquent\Model;
+
+class TestModel extends Model
+{
+  protected $table = "tests";
+}
--- api/public/index.php
+++ api/public/index.php
@@ -1,11 +1,31 @@
 <?php
+use Libsql\Laravel\LibsqlConnection;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 require __DIR__ . "/../vendor/autoload.php";
 
 $app = AppFactory::create();
+
+$capsule = new Capsule();
+
+$capsule->getDatabaseManager()->extend("libsql", function ($config, $name) {
+  $config["name"] = $name;
+  return new LibsqlConnection($config);
+});
+
+$capsule->addConnection([
+  "driver" => "libsql",
+  "url" => $_ENV["TURSO_DATABASE_URL"],
+  "token" => $_ENV["TURSO_AUTH_TOKEN"],
+  "database" => ":memory:",
+  "prefix" => "",
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 $app->get("/hello/{name}", function (
   Request $request,
