b83681262fff36bbdeac50b2dc4067e2e0d6bc60
Carlos Reis <carlosfmreis91@gmail.com>
2026-09-11 19:03:11 +0100

    Fix put requests

--- api/routes/api.php
+++ api/routes/api.php
@@ -9,12 +9,12 @@ use App\Http\Middleware\CheckApiPassword;
 Route::middleware([CheckApiPassword::class])->group(function () {
     Route::get("/animals", [AnimalsController::class, "index"]);
     Route::post("/animals", [AnimalsController::class, "create"]);
-    Route::put("/animals/{id}", [AnimalsController::class, "update"]);
+    Route::put("/animals", [AnimalsController::class, "update"]);
     Route::delete("/animals/{id}", [AnimalsController::class, "delete"]);
 
     Route::get("/bookings", [BookingsController::class, "index"]);
     Route::post("/bookings", [BookingsController::class, "create"]);
-    Route::put("/bookings/{id}", [BookingsController::class, "update"]);
+    Route::put("/bookings", [BookingsController::class, "update"]);
     Route::delete("/bookings/{id}", [BookingsController::class, "delete"]);
 
     Route::get("/owners", [OwnersController::class, "index"]);
